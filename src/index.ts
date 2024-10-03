946a9c5aab87d3974806bca8e9067bdd39a6e8af
Ananth Shetty <ananthshetty203@gmail.com>
2024-10-03 14:04:57 +0530

    added get methods

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
+import {  PrismaClient } from "@prisma/client";
 import { Elysia } from "elysia";
+const db = new PrismaClient();
+const app = new Elysia();
 
-const app = new Elysia()
+app.get("/", () => "Hello Elysia");
+app.get("api", () => {
+  return { name: "Elysia", version: "1.0.0" };
+});
+app.get("api/patients",async () => {
+  let patients = await db.patient.findMany()
+  return patients
+});
 
+app.get("api/patient/:id",async ({ params: { id } }) => {
+  return await db.patient.findFirst({
+    where: {
+      id:id
+    }
+  })
+})
 
-app.get("/", () => "Hello Elysia")
-app.get("api",()=> {name: "Elysia"})
-
+app.post("/api/patient",()=> {
+  return 
+})
 app.listen(3000);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
