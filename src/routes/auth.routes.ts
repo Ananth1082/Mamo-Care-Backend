6ca8707dab771d336c92e448ef4f31cfe0b18a4d
Ananth Shetty <ananthshetty203@gmail.com>
2024-10-09 23:28:34 +0530

    trying to fix 5o2 error

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -11,18 +11,11 @@ import { VerifyOTPReqBody, JWTHeader } from "../types/auth";
 export const authRoutes = () => (app: Elysia) =>
   app.group("/auth", (app) =>
     app
-      .post(
-        "/send-otp",
-        async ({ body, set }) => {
-          set.headers["content-type"] = "text/json";
-          return await sendOTP(body);
-        },
-        {
-          body: t.Object({
-            phone_number: t.String(),
-          }),
-        }
-      )
+      .post("/send-otp", ({ body }) => sendOTP(body), {
+        body: t.Object({
+          phone_number: t.String(),
+        }),
+      })
       .post(
         "/verify-otp",
         ({ body, query, set }) => {
