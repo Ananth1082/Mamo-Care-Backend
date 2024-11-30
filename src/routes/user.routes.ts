db61ca40a454b7fa66347d70d8db80ca28096f80
Ananth1082 <ananthshetty203@gmail.com>
2024-11-30 22:41:17 +0530

    [feat] added get user profile

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,30 +6,40 @@ import { logout } from "../controllers/auth.controllers";
 export const userRoutes = () => (app: Elysia) =>
   app.group("/user", (app) =>
     app
-    .get("/profile/:id", async ({ params }) => {
-      const { id } = params;
-      try {
-        return await getUserProfile(id);
-      } catch (error) {
-        return {
-          msg: "error",
-          error: "Invalid user id",
-        };
-      }
-    })
-    .derive(JWTFilter)
-    .put("/profile",({set,jwt_payload})=>{
-      
-    })
-    .put("/logout", ({ set, jwt_payload }) => {
-      try {
-        set.status = 200;
-        return logout(jwt_payload.session_id);
-      } catch (error) {
-        return {
-          msg: "Logout error",
-          error,
-        };
-      }
-    })
+      .get("/profile/:id", async ({ set, params }) => {
+        const { id } = params;
+        try {
+          return await getUserProfile(id);
+        } catch (error) {
+          set.status = 404;
+          return {
+            msg: "error",
+            error: "Invalid user id",
+          };
+        }
+      })
+      .derive(JWTFilter)
+      .get("/profile", async ({ jwt_payload }) => {
+        const user_id = jwt_payload.user_id;
+        try {
+          return await getUserProfile(user_id);
+        } catch (error) {
+          return {
+            msg: "error",
+            error: "Invalid user id",
+          };
+        }
+      })
+      // .put("/profile", ({ set, jwt_payload }) => {})
+      .put("/logout", ({ set, jwt_payload }) => {
+        try {
+          set.status = 200;
+          return logout(jwt_payload.session_id);
+        } catch (error) {
+          return {
+            msg: "Logout error",
+            error,
+          };
+        }
+      })
   );
